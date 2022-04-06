3736468b27f1022c569cd63bb34b0b65da1a5fef
geer26 <gergo.kurucz@gmail.com>
2022-04-06 14:38:28 +0200

    Initial commit

---
+++ routes.py
@@ -0,0 +1,15 @@
+from main import app
+from models import Item
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    print(dict(item))
+    return item
